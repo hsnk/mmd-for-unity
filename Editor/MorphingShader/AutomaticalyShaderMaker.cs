00c41da0e4e6aa995d4838b3f3a49eb2d75bab4b
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-11-06 02:07:50 +0900

    コードの自動生成用クラス追加

---
+++ Editor/MorphingShader/AutomaticalyShaderMaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+public class AutomaticalyShaderMaker
+{
+	const string property = "{property}";
+	const string directive = "{directive}";		// プロトタイプ宣言
+	const string shader_name = "{shader_name}";
+
+	
+
+	public string Replace(string text, SkinPack[] packs)
+	{
+		return "";
+	}
+}
